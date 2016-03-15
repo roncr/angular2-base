b80b621a5f3dfebe616605105a382f7ca4d0459b
roncr <roncr10b@gmail.com>
2016-03-15 00:39:27 -0700

    Minor improvements to the code

--- tools/gulp/utils/clean.ts
+++ tools/gulp/utils/clean.ts
@@ -4,9 +4,13 @@ import * as rimraf from 'rimraf';
 
 export default function clean(paths: string|string[], done) {
     let pathsArray: string[];
+
     if (!(paths instanceof Array)) {
         pathsArray = [<string>paths];
-    } else pathsArray = paths;
+    } else {
+        pathsArray = paths;
+    }
+
     let promises = pathsArray.map(p => {
         return new Promise(resolve => {
             rimraf(p, e => {
@@ -19,5 +23,6 @@ export default function clean(paths: string|string[], done) {
             });
         });
     });
+
     Promise.all(promises).then(() => done());
 }
